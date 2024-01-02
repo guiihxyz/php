8bf11043bd6d1c43bb10df9bd5ae8d5dea833a05
guiihxyz <127353136+guiihxyz@users.noreply.github.com>
2024-01-02 18:33:27 -0300

    update

---
+++ curso php antigo/ex002/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício PHP</title>
+    <style>
+        * {
+            font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+            font-size: 2em;
+        }
+    </style>
+</head>
+<body>
+    <?php 
+        $idade = 20;
+
+        switch($idade) {
+            
+        }
+    ?>
+</body>
+</html>
