ce97ac2a7ecfa85cdb0ab414d838417de0e4e583
guiihxyz <127353136+guiihxyz@users.noreply.github.com>
2024-01-03 20:28:57 -0300

    update

---
+++ curso php antigo/ex009/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício PHP</title>
+</head>
+<body>
+    <?php 
+        $nome = "José Berrante Animal Oroscopo";
+        echo "$nome <br>";
+        echo str_word_count($nome, 0);
+    ?>
+</body>
+</html>
