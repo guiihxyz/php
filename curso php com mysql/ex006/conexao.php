c5359ddbecf64fb055efef75f76a9b38b57607ab
guiihxyz <127353136+guiihxyz@users.noreply.github.com>
2024-01-07 20:45:09 -0300

    update

---
+++ curso php com mysql/ex006/conexao.php
@@ -0,0 +1,9 @@
+<?php 
+
+    $banco = "localhost";
+    $user = "root";
+    $password = "";
+    $database = "cursosql";
+
+    $conexao = mysqli_connect($banco, $user, $password, $database);
+?>
---
+++ curso php com mysql/ex006/index.php
@@ -0,0 +1,19 @@
+<?php
+    
+    include "conexao.php";
+
+    mysqli_close($conexao);
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício PHP</title>
+</head>
+<body>
+    
+</body>
+</html>
